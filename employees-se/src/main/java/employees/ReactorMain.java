9cfc37a3a0ea74c7d8260d222a90826e0ff143c4
Istvan Viczian <viczian.istvan@gmail.com>
2025-11-24 11:39:53 +0100

    Bevezetés a Mono és Flux használatába.

--- employees-se/src/main/java/employees/ReactorMain.java
+++ employees-se/src/main/java/employees/ReactorMain.java
@@ -1,8 +1,38 @@
 package employees;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class ReactorMain {
 
     public static void main(String[] args) {
+//        Stream.of(new Employee("John", 1980), new Employee("Jane", 1985))
+//                        .forEach(System.out::println);
+
+        // Non blocking backpressure = nem árasztjuk el a fogyasztót
+        String name = Flux.just(new Employee("John", 1980), new Employee("Jane", 1985))
+                .filter(employee -> employee.yearOfBirth() > 1982)
+                .map(Employee::name)
+//                .subscribe(System.out::println);
+                // SOHA NEM HASZNÁLHATJUK!!
+                .blockFirst();
+        // Block metódusok Spring WebFlux alkalmazásban kivételt dobnak!!!!
+        System.out.println(name);
+
+        Flux.just(new Employee("John", 1980), new Employee("Jane", 1985))
+                .filter(employee -> employee.yearOfBirth() > 1982)
+                        .singleOrEmpty()
+                                .subscribe(System.out::println);
+
+//        Optional.of(new Employee("John", 1980)).stream().forEach(System.out::println);
+
+        Mono.just(new Employee("John", 1980))
+                .subscribe(System.out::println);
+
+
 
     }
 }
