b905f600191a2ff9efdb4d644cd32f5e154b2b68
Istvan Viczian <viczian.istvan@gmail.com>
2025-11-25 15:01:47 +0100

    WebClient kiindulási állapot

---
+++ courses-webclient/src/main/java/training/courseswebclient/CoursesWebclientApplication.java
@@ -0,0 +1,13 @@
+package training.courseswebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesWebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesWebclientApplication.class, args);
+    }
+
+}
---
+++ courses-webclient/src/test/java/training/courseswebclient/CoursesWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.courseswebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesWebclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
