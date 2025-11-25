b20b96fcd3079bc076a47ef3efa9e194d0628c63
Istvan Viczian <viczian.istvan@gmail.com>
2025-11-25 13:47:38 +0100

    MongoDB @Query használata példa

--- employees-mongodb/src/main/java/employees/EmployeeRepository.java
+++ employees-mongodb/src/main/java/employees/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package employees;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,5 +10,10 @@ public interface EmployeeRepository extends ReactiveCrudRepository<Employee, Str
     Flux<EmployeeDto> findAllBy() ;
 
     <T> Mono<T> findDtoById(String id, Class<T> clazz);
+
+//    @Query("""
+//        { 'name' : ?0, 'year_of_birth' : {$gte: ?#{#name}} }
+//        """)
+    Flux<EmployeeDto> findByName(String name);
 
 }
