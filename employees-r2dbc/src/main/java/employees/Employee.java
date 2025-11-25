74b19ca6fd84b509341db416187d7f7fbb21b1b3
Istvan Viczian <viczian.istvan@gmail.com>
2025-11-25 10:21:38 +0100

    Adatbázis bevezetése

--- employees-r2dbc/src/main/java/employees/Employee.java
+++ employees-r2dbc/src/main/java/employees/Employee.java
@@ -2,11 +2,15 @@ package employees;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @AllArgsConstructor
+@Table("employees")
 public class Employee {
 
+    @Id
     private Long id;
 
     private String name;
